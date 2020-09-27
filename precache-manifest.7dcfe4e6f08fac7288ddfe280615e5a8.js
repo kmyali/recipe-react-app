d64dcdfc7b8c57a658479880889ef58bde802535
kmyali <kmali@mun.ca>
2020-09-27 20:38:14 -0230

    Updates

---
+++ precache-manifest.7dcfe4e6f08fac7288ddfe280615e5a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491370f6b7dd429dc0b1d8f660c3a515",
+    "url": "/recipe-react-app/index.html"
+  },
+  {
+    "revision": "daba66a2fa2d39edd0ad",
+    "url": "/recipe-react-app/static/css/main.70cb1d0b.chunk.css"
+  },
+  {
+    "revision": "80329ef14adcff01bc72",
+    "url": "/recipe-react-app/static/js/2.0a75d31b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-react-app/static/js/2.0a75d31b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daba66a2fa2d39edd0ad",
+    "url": "/recipe-react-app/static/js/main.04415d3a.chunk.js"
+  },
+  {
+    "revision": "06c5e40a4cc4ae805eee",
+    "url": "/recipe-react-app/static/js/runtime-main.ee7b3bcb.js"
+  }
+]);
